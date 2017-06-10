42bab05a366532c900697ecd67c4cff80077fc43
Lou <luana.martelli@heig-vd.ch>
2017-06-10 17:27:25 +0200

    Add bonus step. Think it's working

--- docker/apache-reverse-proxy/template/config-template.php
+++ docker/apache-reverse-proxy/template/config-template.php
@@ -1,22 +1,28 @@
 <?php
-  $dynamic_app = getenv('DYNAMIC_APP');
-  $static_app = getenv('STATIC_APP');
+  $dynamic_app1 = getenv('DYNAMIC_APP1');
+  $dynamic_app2 = getenv('DYNAMIC_APP2');
+  $static_app1 = getenv('STATIC_APP1');
+  $static_app2 = getenv('STATIC_APP2');
 ?>
 <VirtualHost *:80>
   ServerName demo.res.ch
-
-  <Proxy "balancer://dynamicapp">
-    BalancerMember "http://<?php print "$dynamic_app";?>"
-  </Proxy>
   
-  <Proxy "balancer://staticapp">
-      BalancerMember "http://<?php print "$static_app";?>"
+  <Proxy balancer://staticapp>
+      BalancerMember "http://<?php print "$static_app1"?>"
+      BalancerMember "http://<?php print "$static_app2"?>"
+    ProxySet lbmethod=byrequests
   </Proxy>
 
-  ProxyPass '/api/students/' 'balancer://dynamicapp/api/students/'
-  ProxyPassReverse '/api/students/' 'balancer://dynamicapp/api/students/'
+  <Proxy balancer://dynamicapp>
+    BalancerMember "http://<?php print "$dynamic_app1"?>"
+    BalancerMember "http://<?php print "$dynamic_app2"?>"
+    ProxySet lbmethod=byrequests
+  </Proxy>
 
-  ProxyPass        '/' 'balancer://staticapp/'
+  ProxyPass '/api/students/' 'balancer://dynamicapp/'
+  ProxyPassReverse '/api/students/' 'balancer://dynamicapp/'
+
+  ProxyPass '/' 'balancer://staticapp/'
   ProxyPassReverse '/' 'balancer://staticapp/'
  
 </VirtualHost>
